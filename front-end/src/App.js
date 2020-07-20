ddbe1254983e89eba5784a5b388ac0fa6388bb4d
Paddy <pacurtin@tcd.ie>
2020-07-20 11:31:39 -0500

    Table style done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,9 +5,20 @@ import PersonsTable from "./PersonsTable";
  */
 
 function App() {
+
+  function createData(name, email, phone) {
+    return { name, email, phone };
+  }
+
+  const persons = [
+    createData('Jimmy', "Jim@gmail.com", "5041234567"),
+    createData('John', "John@gmail.com", "5047654321"),
+    createData('Reginald', "Reginald@gmail.com", "5041234123")
+  ];
+
   return (
     <div className="App">
-      <PersonsTable persons={[]}/>
+      <PersonsTable persons={persons}/>
     </div>
   );
 }
--- front-end/src/PersonsTable.js
+++ front-end/src/PersonsTable.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    overflowX: 'hide',
+  },
+  tableCell: {
+    paddingRight: 4,
+    paddingLeft: 4
+  }
+});
 
 function PersonsTable(props) {
-  return <h1>TABLE!!!!</h1>;
+  const classes = useStyles();
+  const {persons} = props;
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {persons.map((person) => (
+            <TableRow key={person.email}>
+              <TableCell>{person.name}</TableCell>
+              <TableCell>{person.email}</TableCell>
+              <TableCell>{person.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default PersonsTable;
